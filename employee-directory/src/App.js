430b91ea6a03564ec224902ad29d700184d49555
Justin Suderman <jsuderman1@gmail.com>
2020-09-05 13:02:23 -0400

    added compontent files for organization

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import Header from "./components/Header";
+// import Container from "./components/Container"
 import Directory from "./pages/Directory";
 
 function App() {
